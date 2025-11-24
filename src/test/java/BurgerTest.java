9038c06a59ba86829bed79af7713a56a11642dcf
Alexandra Rensh <alexandra.rnsh@yandex.ru>
2025-11-24 22:07:23 +0300

    review fixes 1

--- src/test/java/BurgerTest.java
+++ src/test/java/BurgerTest.java
@@ -10,90 +10,117 @@ import praktikum.Ingredient;
 import praktikum.IngredientType;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BurgerTest {
 
 
-        @Mock
-        Bun bunMock;
+    @Mock
+    Bun bunMock;
 
-        @Mock
-        Ingredient ingredientMock1;
+    @Mock
+    Ingredient sauceIngredient;
 
-        @Mock
-        Ingredient ingredientMock2;
+    @Mock
+    Ingredient fillingIngredient;
 
-        Burger burger;
+    Burger burger;
 
-        @Before
-        public void init() {
-            MockitoAnnotations.initMocks(this);
-            burger = new Burger();
-        }
-
-        @Test
-        public void testSetBuns() {
-            burger.setBuns(bunMock);
-            assertEquals(bunMock, burger.bun);
-        }
-
-        @Test
-        public void testAddIngredient() {
-            burger.addIngredient(ingredientMock1);
-            assertEquals(1, burger.ingredients.size());
-            assertEquals(ingredientMock1, burger.ingredients.get(0));
-        }
-
-    @Test
-    public void testRemoveIngredient() {
-        System.out.println("Step 1: ingredients = " + burger.ingredients);
-
-        burger.addIngredient(ingredientMock1);
-        System.out.println("Step 2 (added ingredientMock1): ingredients = " + burger.ingredients);
-
-        burger.addIngredient(ingredientMock2);
-        System.out.println("Step 3 (added ingredientMock2): ingredients = " + burger.ingredients);
-
-        burger.removeIngredient(0);
-        System.out.println("Step 4 (after remove index 0): ingredients = " + burger.ingredients);
-
-        assertEquals(1, burger.ingredients.size());
-        assertEquals(ingredientMock2, burger.ingredients.get(0));
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+        burger = new Burger();
     }
 
-        @Test
-        public void testMoveIngredient() {
-            burger.addIngredient(ingredientMock1);
-            burger.addIngredient(ingredientMock2);
+    @Test
+    public void testSetBuns() {
+        burger.setBuns(bunMock);
+        assertEquals(bunMock, burger.bun);
+    }
 
-            burger.moveIngredient(0, 1);
+    @Test
+    public void testAddIngredientSize() {
+        burger.addIngredient(sauceIngredient);
+        assertEquals(1, burger.ingredients.size());
+    }
 
-            assertEquals(ingredientMock2, burger.ingredients.get(0));
-            assertEquals(ingredientMock1, burger.ingredients.get(1));
-        }
+    @Test
+    public void testAddIngredientValue() {
+        burger.addIngredient(sauceIngredient);
+        assertEquals(sauceIngredient, burger.ingredients.get(0));
+    }
 
-        @Test
-        public void testGetReceiptFormatsCorrectly() {
+    @Test
+    public void testRemoveIngredientReducesSize() {
+        burger.addIngredient(sauceIngredient);
+        burger.addIngredient(fillingIngredient);
 
-            when(bunMock.getName()).thenReturn("black bun");
-            when(bunMock.getPrice()).thenReturn(100f);
+        burger.removeIngredient(0);
 
-            when(ingredientMock1.getName()).thenReturn("hot sauce");
-            when(ingredientMock1.getType()).thenReturn(IngredientType.SAUCE);
-            when(ingredientMock1.getPrice()).thenReturn(50f);
+        assertEquals(1, burger.ingredients.size());
+    }
 
-            burger.setBuns(bunMock);
-            burger.addIngredient(ingredientMock1);
+    @Test
+    public void testRemoveIngredientKeepsCorrectElement() {
+        burger.addIngredient(sauceIngredient);
+        burger.addIngredient(fillingIngredient);
 
-            String receipt = burger.getReceipt();
-            System.out.println("RECEIPT:\n" + receipt);
+        burger.removeIngredient(0);
 
-            assertTrue(receipt.contains("(==== black bun ====)"));
-            assertTrue(receipt.contains("= sauce hot sauce ="));
-            assertTrue(receipt.contains("Price: 250,000000"));
-        }
+        assertEquals(fillingIngredient, burger.ingredients.get(0));
+    }
+
+
+    @Test
+    public void testMoveIngredientNewFirstPosition() {
+        burger.addIngredient(sauceIngredient);
+        burger.addIngredient(fillingIngredient);
+
+        burger.moveIngredient(0, 1);
+
+        assertEquals(fillingIngredient, burger.ingredients.get(0));
+    }
+
+    @Test
+    public void testMoveIngredientNewSecondPosition() {
+        burger.addIngredient(sauceIngredient);
+        burger.addIngredient(fillingIngredient);
+
+        burger.moveIngredient(0, 1);
+
+        assertEquals(sauceIngredient, burger.ingredients.get(1));
+    }
+
+
+    @Test
+    public void testGetReceiptFormatsCorrectly() {
+
+        when(bunMock.getName()).thenReturn("black bun");
+        when(bunMock.getPrice()).thenReturn(100f);
+
+        when(sauceIngredient.getName()).thenReturn("hot sauce");
+        when(sauceIngredient.getType()).thenReturn(IngredientType.SAUCE);
+        when(sauceIngredient.getPrice()).thenReturn(50f);
+
+        burger.setBuns(bunMock);
+        burger.addIngredient(sauceIngredient);
+
+        String receipt = burger.getReceipt();
+        System.out.println("[" + receipt + "]");
+
+        String expected =
+                "(==== black bun ====)\n" +
+                        "= sauce hot sauce =\n" +
+                        "(==== black bun ====)\n\n" +
+                        "Price: 250,000000\n";
+        System.out.println("[" + expected + "]");
+
+        assertEquals(
+                expected.replace("\r", "").trim(),
+                receipt.replace("\r", "").trim()
+        );
+
+    }
 
 }
